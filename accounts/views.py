9b7891afba9789e80d7d22600ef08ce504a4de51
Ubejd Jashari <j.ubejd@gmail.com>
2022-12-20 13:55:25 +0100

    user account activation and Activation Link Expiry

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from email.message import EmailMessage
+from multiprocessing import AuthenticationError
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import Account
+from .forms import RegistrationForm
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import  login_required
 
 # Create your views here.
+
+
+#VERIFICATION EMAIL
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name = last_name, email=email, username =  username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            #USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = "Please activate your account"
+            message = render_to_string("accounts/account_verification_email.html", {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email adress. Please verify it')
+            return redirect ('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render (request, 'accounts/register.html', context)
+
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credintials')
+            return redirect('login')
+    return render (request, 'accounts/login.html')
+
+@login_required(login_url = "login")
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You are logged out!")
+    return redirect("login")
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'congratulations! Your account is activated.')
+        return redirect ('login')
+    else:
+        messages.ERROR(request, 'Invalid activation link')
+        return redirect('register')
+        
+@login_required(login_url = "login")
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,3 +135,19 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media/'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+
+#SMTIP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'j.ubejd@gmail.com'
+EMAIL_HOST_PASSWORD = 'uyhjvknofvnspayq'
+EMAIL_USE_TLS = 'True'
